679845be7ce7829b8b20c08d1e335b0aed4515a2
Abdul Khan <akhan17@outlook.com>
2020-07-27 00:05:42 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,6 +12,16 @@ before_action :set_song, only: [:show, :edit, :update, :destroy]
   def edit
   end
 
+  def create
+  @song = Song.new(song_params)
+  if @song.valid?
+    @song.save
+    redirect_to song_path(@song)
+  else
+    render :new
+  end
+end
+
   private
 
 def set_song
